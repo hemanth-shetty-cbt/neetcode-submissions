52026c7e481bcf548211e132de860c6f91130b6a
HEMANTH U SHETTY <126490029+hemanth-shetty-cbt@users.noreply.github.com>
2026-08-27 00:07:08 +0530

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+            (a,b) -> ((b[0] * b[0] + b[1] * b[1]) - (a[0]*a[0] + a[1]*a[1]))
+        );
+
+        for(int[] arr:points) {
+
+            pq.offer(arr);
+
+            if(pq.size() >k) {
+                pq.poll();
+            }
+
+        }
+
+        int result[][] = new int[k][2];
+
+        for (int i=0;i<k;i++) {
+            result[i] = pq.poll();
+        }
+
+        return result;
+    }
+}
