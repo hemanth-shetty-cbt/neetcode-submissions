5a7bb4e1e21a97308501a2d440c96329e36e1f87
HEMANTH U SHETTY <126490029+hemanth-shetty-cbt@users.noreply.github.com>
2026-08-30 18:45:46 +0530

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        int n = temperatures.length;
+
+        int result[] = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for (int i=n-1; i>=0; i--) {
+
+
+            while(!st.isEmpty() && temperatures[i] >= temperatures[st.peek()]) {
+
+                st.pop();
+            }
+
+            //bigger element still exist
+            //then we add the value from the stack - the current index  we will get the next greater element present.
+            if(!st.isEmpty()) {
+                result[i] = st.peek() - i;
+            }
+            st.push(i);
+
+        }
+
+        return result;
+
+        
+    }
+}
