a47b5b63fa17dc6edebde06ec1438668958ea620
HEMANTH U SHETTY <126490029+hemanth-shetty-cbt@users.noreply.github.com>
2026-09-24 10:29:12 +0530

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        
+
+        int count =0 , candidate = 0;
+
+        for (int num: nums) {
+
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (candidate == num ) ? 1:-1;
+
+        }
+        return candidate;
+    }
+}
