57008df0f0f3f788521669073c5e9227e49726bf
HEMANTH U SHETTY <126490029+hemanth-shetty-cbt@users.noreply.github.com>
2026-09-24 10:19:32 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+
+        Map<Integer, Integer> mp = new HashMap<>();
+
+        int n = nums.length;
+
+        for(int num: nums) {
+
+            mp.put(num, mp.getOrDefault(num, 0) +1);
+
+            if (mp.get(num) > n/2) {
+                return num;
+            }
+
+
+        }
+
+        return -1;
+
+        
+    }
+}
